6d4f5ca3a5b7dd0a5592ee2f19670d34c74a7363
Yohann <yoyo24du26@gmail.com>
2026-02-02 11:13:18 +0100

    feat(DB): adding configuration to connect nest.js to the db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StagesModule } from './module/stages/stages.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { buildTypeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [StagesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        buildTypeOrmConfig(configService),
+    }),
+    StagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
